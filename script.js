33bf977ad3247fe8ec5bacfe089e7202e515ff5a
“Will9Costa” <wildembergdcosta@gmail.com>
2024-02-10 12:12:06 -0300

    Ajuste das cores, funções js

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ function toggleColor(element, color) {
     var paragraphs = document.querySelectorAll('#container p');
     paragraphs.forEach(function(p) {
         p.classList.remove('active');
+        p.style.backgroundColor = '';
     });
 
     element.classList.add('active');
     element.style.backgroundColor = color;
     document.body.style.backgroundColor = color;
+}
+
+function resetColors() {
+    var paragraphs = document.querySelectorAll('#container p');
+    paragraphs.forEach(function(p) {
+        p.classList.remove('active');
+        p.style.backgroundColor = ''; // Restaura o background original
+    });
+
+    // Reseta o background do body
+    document.body.style.backgroundColor = ' lightcoral'; // Cor padrão
 }
