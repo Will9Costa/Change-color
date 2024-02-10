312df64273188ae2d669e383e2d3e66db568c305
“Will9Costa” <wildembergdcosta@gmail.com>
2024-02-10 09:45:34 -0300

    Retirando nomes das tags p

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function toggleMode(color) {
-document.body.style.backgroundColor = color;  
+function toggleColor(element, color) {
+    var paragraphs = document.querySelectorAll('#container p');
+    paragraphs.forEach(function(p) {
+        p.classList.remove('active');
+    });
+
+    element.classList.add('active');
+    element.style.backgroundColor = color;
+    document.body.style.backgroundColor = color;
 }
